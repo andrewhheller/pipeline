8a767f9637897e414c56c0a813d4a74f266143ce
Daniel Seeley <Daniel.h.seeley@gmail.com>
2018-07-14 18:05:10 -0400

    Started Async section

---
+++ aSyncPipeline.js
@@ -0,0 +1,8 @@
+//Accept addFromFile - get the value of the file
+//Determine data structure of Arrays for reduce
+
+const pipelineAsync = (str, fn) => {
+
+};
+
+module.exports = pipelineAsync;
--- test/pipeline-specs.js
+++ test/pipeline-specs.js
@@ -1,6 +1,8 @@
 const expect = require('chai').expect;
-
 const pipeline = require('../pipeline.js');
+const pipelineAsync = require('../aSyncPipeline.js');
+
+
 
 describe('it is a function', () => {
 
@@ -8,9 +10,21 @@ describe('it is a function', () => {
     expect(pipeline).be.a('function');
   });
 
-  it('pipeline takes a string and returns an array', () => {
+  it('pipeline takes a string and returns an integer', () => {
     expect(pipeline('set 1 | add 1 | mult 21')).to.be.a('number');
     expect(pipeline('set 1 | mult 10 | mult 4 | add 3')).to.equal(43);
   });
 
+});
+
+describe('pipelineAsync is: ', ()=> {
+  it('is a function', ()=> {
+    expect(pipelineAsync).be.a('function');
+  });
+  it('takes a string and returns an integer', ()=> {
+    expect(pipelineAsync('set 0, addFromeFile /files/foo.txt | add 1 | mult 21', (err, result)=> {
+      console.log(err);
+      console.log(result);
+    })).to.equal(42);
+  });
 });
